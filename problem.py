94e9d5c29ab495a6df0a946bda3fe7f600aa1de1
BENNANI MEHDY p1711302 <mehdy.bennani@etu.univ-lyon1.fr>
2022-03-16 21:23:55 +0100

    first commit

---
+++ problem.py
@@ -0,0 +1,43 @@
+import os
+import pandas as pd
+import rampwf as rw
+from itertools import combinations_with_replacement
+from sklearn.model_selection import ShuffleSplit
+
+problem_title = 'Covid Vaccince Prediction'
+
+_target_names = ['Vaccine', 'Business']
+
+_prediction_label_ = combinations_with_replacement(
+                                ['Completely disagree', 
+                                'Somewhat disagree', 
+                                'No opinion', 
+                                'Completely agree', 
+                                'Somewhat agree'],2)
+
+Predictions = rw.prediction_types.make_regression(label_names=_target_names)
+workflow = rw.workflows.Regressor()
+
+score_types = rw.score_types.Accuracy(name= 'acc')
+
+def get_cv(X, y):
+    cv = ShuffleSplit(n_splits=10, test_size=0.25, random_state=57)
+    return cv.split(X, y)
+
+def _get_data(path, split):
+    split_path = os.path.join(path, "data", split, "*.csv")
+    data = pd.read_csv(split_path, encoding='iso-8859-1')
+
+    X = data.drop(['Vaccine','Business2'], axis=1)
+    Y = data[['Vaccine','Business2']].values
+    return X, Y
+
+def get_train_data(path):
+    return _get_data(path, 'test')
+
+def get_test_data(path):
+    return _get_data(path, 'test')
+
+
+
+
