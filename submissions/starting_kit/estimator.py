8751de73b9454997026f671028bbef740ae8b69b
Thomas Boyer <thomas.boyer@ensta-paris.fr>
2023-02-20 20:09:00 +0100

    Use classifier from grid search (indeed much better)

--- submissions/starting_kit/estimator.py
+++ submissions/starting_kit/estimator.py
@@ -7,7 +7,14 @@ def get_estimator():
     """TODO"""
     # TODO: add pipeline for data cleaning like in https://github.com/ramp-kits/titanic/blob/master/submissions/random_forest_20_5/estimator.py
     model = RandomForestClassifier(
-        n_jobs=-1, random_state=42, class_weight="balanced_subsample"
+        n_jobs=-1,
+        random_state=42,
+        class_weight="balanced_subsample",
+        criterion="gini",
+        max_depth=6,
+        min_samples_leaf=1,
+        min_samples_split=3,
+        n_estimators=50,
     )
 
     return model
