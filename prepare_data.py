a58cb4881c56ae6c74371bcac5fa1c6d55c1a9bb
davedhn <98386206+davedhn@users.noreply.github.com>
2023-02-23 21:25:11 +0100

    Delete prepare_data.py

--- prepare_data.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-def prepare_data(X):
-    labels = X["fire"]
-    data = X.drop(columns="fire")
-
-    data["Date"] = pd.to_datetime(data["Date"])
-    labels = labels.astype("category")
-    return data, labels
